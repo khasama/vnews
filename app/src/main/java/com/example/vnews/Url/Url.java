3806d85772065747e716e60eebf5bfe2ad95f964
lehuynhanhtai123 <lehuynhanhtai123@gmail.com>
2021-05-15 15:41:06 +0700

    taipro

--- app/src/main/java/com/example/vnews/Url/Url.java
+++ app/src/main/java/com/example/vnews/Url/Url.java
@@ -6,6 +6,7 @@ public class Url {
     public static String domain = "http://tnews.ga/";
     public static String Banner = domain + "api/slide.php";
     public static String News = domain + "api/news.php";
+    public static String HotNews = domain + "api/hotnews.php";
     public static String Content = domain + "api/content.php?idTin=";
     public static String Tinlienquan = domain + "api/tinlienquan.php?idLoaiTin=";
     public static String getTechNews(String idLoaitin){
--- app/src/main/java/com/example/vnews/fragmentJ/FragmentHotnews.java
+++ app/src/main/java/com/example/vnews/fragmentJ/FragmentHotnews.java
@@ -1,6 +1,9 @@
 package com.example.vnews.fragmentJ;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,15 +11,79 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.vnews.R;
+import com.example.vnews.Url.Url;
+import com.example.vnews.adapterJ.NewsAdapter;
+import com.example.vnews.modelJ.News;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class FragmentHotnews extends Fragment {
+    private Context mContext;
+    RecyclerView rvTinHot;
+    ArrayList<News> arrayNews;
+    NewsAdapter newsAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        Log.d("asd", "hot");
+        Log.d("asd", "hotss");
         View view = inflater.inflate(R.layout.fragment_hotnews, container, false);
+        anhxa(view);
+        getDataTH();
         return view;
+    }
+
+    private void getDataTH() {
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Url.HotNews, response -> {
+            if(response != null){
+                String id = "";
+                String Title = "";
+                String NgayDang = "";
+                String Nguon = "";
+                String Anh = "";
+                String Loai = "";
+                for(int i = 0; i < response.length(); i++){
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        id = jsonObject.getString("id");
+                        Title = jsonObject.getString("title");
+                        NgayDang = jsonObject.getString("ngaydang");
+                        Nguon = jsonObject.getString("nguon");
+                        Anh = jsonObject.getString("img");
+                        Loai = jsonObject.getString("loai");
+                        arrayNews.add(new News(id, Title, NgayDang, Nguon, Loai, Anh));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                rvTinHot.setAdapter(newsAdapter);
+            }
+        }, error -> Log.e("aaa", error.toString()));
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void anhxa(View view) {
+        rvTinHot = view.findViewById(R.id.rvTinHot);
+        arrayNews = new ArrayList<>();
+        newsAdapter = new NewsAdapter(getContext(), arrayNews);
+        rvTinHot.setHasFixedSize(true);
+        rvTinHot.setLayoutManager(new GridLayoutManager(getContext(), 1));
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 }
