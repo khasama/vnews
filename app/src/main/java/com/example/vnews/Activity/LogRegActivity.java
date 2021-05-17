e1b0fa35f2c1d0578172416a6ba9a84c4201acf6
khasama <khavl741953@gmail.com>
2021-05-17 13:48:20 +0700

    add fragment login and register

--- app/src/main/java/com/example/vnews/Activity/LogRegActivity.java
+++ app/src/main/java/com/example/vnews/Activity/LogRegActivity.java
@@ -1,6 +1,8 @@
 package com.example.vnews.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,27 +11,37 @@ import android.preference.PreferenceManager;
 import android.view.View;
 
 import com.example.vnews.R;
+import com.example.vnews.adapterJ.FragmentAdapter;
+import com.example.vnews.fragmentJ.FragmentLogin;
+import com.example.vnews.fragmentJ.FragmentRegister;
+import com.google.android.material.tabs.TabLayout;
 
 public class LogRegActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_reg);
-        initPreferences();
+        anhXa();
         init();
     }
 
-    private void initPreferences() {
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        editor = sharedPreferences.edit();
+    private void anhXa() {
+        tabLayout = findViewById(R.id.mytablayout);
+        viewPager = findViewById(R.id.myviewpager);
     }
 
+
     private void init() {
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragmentAdapter.addFragment(new FragmentLogin(), "Đăng nhập");
+        fragmentAdapter.addFragment(new FragmentRegister(), "Đăng ký");
+        viewPager.setAdapter(fragmentAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     public void returnHome(View view){
---
+++ app/src/main/java/com/example/vnews/fragmentJ/FragmentLogin.java
@@ -0,0 +1,30 @@
+package com.example.vnews.fragmentJ;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.vnews.R;
+
+public class FragmentLogin extends Fragment {
+    private Context mContext;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext=context;
+    }
+}
