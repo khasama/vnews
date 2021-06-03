7d5d9d36350eaac218ee02ea5fc3924af7bb4e3c
DESKTOP-QO272N7\huynh <huynhtinnhan2019@gmail.com>
2021-06-04 02:09:01 +0700

    Sad

--- app/src/main/java/com/example/vnews/fragmentJ/FragmentProfile.java
+++ app/src/main/java/com/example/vnews/fragmentJ/FragmentProfile.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
